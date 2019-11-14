4bd6c542b181745724cfecb7c7828a555f8f841a
Asiful Choudhury <asiful307@gmail.com>
2019-11-14 20:16:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,31 @@
-class Author 
+class Author
+  attr_accessor :name
+
   @@all = []
-  
-  attr_accessor :name 
-  
+
   def initialize(name)
-    @name = name 
+    @name = name
     @@all << self
-  end 
-  
+  end
+
   def self.all
     @@all
-  end 
-  
- def posts 
-   posts.all.select {|post| post.Author == self}
- end 
- 
- def add_posts(posts)
-   posts.author = self
- end 
- 
- def add_post_by_title(title)
-   posts = Post.new(title)
-   posts.author = self
- end 
- 
- def self.post_count
-   posts.all.count 
- end 
-end 
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+end
